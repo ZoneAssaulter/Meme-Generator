5d8caa01527bc95a9ea43e267ddec2528b36d399
Baruch <baruch.rozenberg93@gmail.com>
2022-04-16 11:31:36 +0300

    fixed the delete bug

--- js/controllers/saved.controller.js
+++ js/controllers/saved.controller.js
@@ -1,33 +1,31 @@
-'use strict'
+'use strict';
 
 function initSaved() {
-  loadMemes()
-  renderSaved()
+  loadMemes();
+  renderSaved();
 }
 
 function renderSaved() {
-  const memesData = getMemesData()
-  let savedHTMLs = []
-  if (!memesData.length) savedHTMLs = [`<p>No saved photos yet</p>`]
+  const memesData = getMemesData();
+  let savedHTMLs = [];
+  if (!memesData.length) savedHTMLs = [`<p>No saved photos yet</p>`];
   else
     savedHTMLs = memesData.map(
       (memeData, i) =>
-        `<div class="gallery-img"><img src="${memeData}" onclick="onOpenSaved(this.src)"><button class="delete-saved-btn fas fa-trash-alt fa-2x" onclick=onDeleteSaved(${i})"></button></div>`
-    )
-    document.querySelector('.saved .images-container').innerHTML = savedHTMLs.join('')
+        `<div class="gallery-img"><img src="${memeData}" onclick="onOpenSaved(this.src)"><button class="delete-saved-btn fas fa-trash-alt fa-2x" onclick="onDeleteSaved(${i})"></button></div>`
+    );
+  document.querySelector('.saved .images-container').innerHTML =
+    savedHTMLs.join('');
 }
 
 function onOpenSaved(savedSrc) {
-    document.querySelector('.test-img').src = savedSrc
-    resetMeme()
-    onImgSelect(0)
+  document.querySelector('.test-img').src = savedSrc;
+  resetMeme();
+  onImgSelect(0);
 }
 
-function onDeleteSaved(savedIdx){
-  console.log('deleting');
-  event.stopPropagation()
-  console.log('deleting');
-    if (!confirm('Are you sure?')) return
-    deleteMeme(savedIdx)
-    renderSaved()
+function onDeleteSaved(savedIdx) {
+  if (!confirm('Are you sure?')) return;
+  deleteMeme(savedIdx);
+  renderSaved();
 }
